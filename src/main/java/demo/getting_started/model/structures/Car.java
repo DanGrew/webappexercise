2f3ade417fd7d2aa8ab73cb45b23ab035dae8b2c
Dan Grew <danielanthonygrew@gmail.com>
2021-06-11 22:13:24 +0100

    TASK: Model updated to support colours.

--- src/main/java/demo/getting_started/model/structures/Car.java
+++ src/main/java/demo/getting_started/model/structures/Car.java
@@ -1,71 +1,84 @@
 package demo.getting_started.model.structures;
 
+import javafx.scene.paint.Color;
+
 public class Car {
 
-	private Integer id;
-	private String model;
-	private String make;
-	private String preview;
-	private String description;
-	private Integer price;
+   private Integer id;
+   private String model;
+   private String make;
+   private String preview;
+   private String description;
+   private Integer price;
+   private Color colour;
 
-	public Car() {
-	}
+   public Car() {
+      this(null, null, null, null, null, null, null);
+   }
 
-	public Car(Integer id, String model, String make, String description, String preview, Integer price) {
-		this.id = id;
-		this.model = model;
-		this.make = make;
-		this.preview = preview;
-		this.description = description;
-		this.price = price;
-	}
+   public Car(Integer id, String model, String make, String description, String preview, Integer price, Color colour) {
+      this.id = id;
+      this.model = model;
+      this.make = make;
+      this.preview = preview;
+      this.description = description;
+      this.price = price;
+      this.colour = colour;
+   }
 
-	public Integer getId() {
-		return id;
-	}
+   public Integer getId() {
+      return id;
+   }
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+   public void setId(Integer id) {
+      this.id = id;
+   }
 
-	public String getMake() {
-		return make;
-	}
+   public String getMake() {
+      return make;
+   }
 
-	public void setMake(String make) {
-		this.make = make;
-	}
+   public void setMake(String make) {
+      this.make = make;
+   }
 
-	public String getPreview() {
-		return preview;
-	}
+   public String getPreview() {
+      return preview;
+   }
 
-	public void setPreview(String preview) {
-		this.preview = preview;
-	}
+   public void setPreview(String preview) {
+      this.preview = preview;
+   }
 
-	public String getDescription() {
-		return description;
-	}
+   public String getDescription() {
+      return description;
+   }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+   public void setDescription(String description) {
+      this.description = description;
+   }
 
-	public Integer getPrice() {
-		return price;
-	}
+   public Integer getPrice() {
+      return price;
+   }
 
-	public void setPrice(Integer price) {
-		this.price = price;
-	}
+   public void setPrice(Integer price) {
+      this.price = price;
+   }
 
-	public String getModel() {
-		return model;
-	}
+   public String getModel() {
+      return model;
+   }
 
-	public void setModel(String model) {
-		this.model = model;
-	}
+   public void setModel(String model) {
+      this.model = model;
+   }
+
+   public Color getColour() {
+      return colour;
+   }
+
+   public void setColour(Color colour) {
+      this.colour = colour;
+   }
 }
