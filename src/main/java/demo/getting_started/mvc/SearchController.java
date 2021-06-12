631f05660d26aa7d7566ff41763bb25172fd51d9
Dan Grew <danielanthonygrew@gmail.com>
2021-06-12 09:24:54 +0100

    TASK: Fixed issue where the detail would not be hidden. Also extended this functionality to retain the current selection if the search results contain the previously selected. Refactored tests to make test package structure match that of the src. Tested SearchController.

--- src/main/java/demo/getting_started/mvc/SearchController.java
+++ src/main/java/demo/getting_started/mvc/SearchController.java
@@ -1,72 +1,201 @@
 package demo.getting_started.mvc;
 
 
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.select.SelectorComposer;
 import org.zkoss.zk.ui.select.annotation.Listen;
 import org.zkoss.zk.ui.select.annotation.Wire;
-import org.zkoss.zul.Image;
-import org.zkoss.zul.Label;
-import org.zkoss.zul.ListModelList;
-import org.zkoss.zul.Listbox;
-import org.zkoss.zul.Textbox;
-import org.zkoss.zul.ext.Selectable;
+import org.zkoss.zul.*;
 
 import demo.getting_started.model.structures.Car;
 import demo.getting_started.model.services.CarService;
 import demo.getting_started.model.services.CarServiceImpl;
 
-public class SearchController extends SelectorComposer<Component> {
+import static java.util.Collections.singleton;
+
+/**
+ * Provides the controls for searching data and providing results.
+ */
+public class SearchController extends SelectorComposer< Component > {
 
    private static final long serialVersionUID = 1L;
 
-	@Wire
-	private Textbox keywordBox;
-	@Wire
-	private Listbox carListbox;
-	@Wire
-	private Label modelLabel;
-	@Wire
-	private Label makeLabel;
-	@Wire
-	private Label priceLabel;
-	@Wire
-	private Label descriptionLabel;
-	@Wire
-	private Image previewImage;
-	@Wire
-	private Component detailBox;
+   @Wire
+   private Textbox keywordBox;
+   @Wire
+   private Listbox carListbox;
+   @Wire
+   private Label modelLabel;
+   @Wire
+   private Label makeLabel;
+   @Wire
+   private Label priceLabel;
+   @Wire
+   private Label descriptionLabel;
+   @Wire
+   private Image previewImage;
+   @Wire
+   private Component detailBox;
 
+   private final CarService carService;
 
-   private CarService carService = new CarServiceImpl();
+   /**
+    * Constructs a new {@link SearchController}.
+    */
+   public SearchController() {
+      this( new CarServiceImpl() );
+   }
 
-   @Listen("onClick = #searchButton")
-	public void search(){
+   /**
+    * Constructs a new {@link SearchController}.
+    * @param carService for data access.
+    */
+   SearchController( CarService carService ) {
+      this.carService = carService;
+   }
+
+   /**
+    * Performs a search through car data based on the input in the keyword box.
+    */
+   @Listen( "onClick = #searchButton" )
+   public void search() {
       String keyword = keywordBox.getValue();
-      List<Car> result = carService.search(keyword);
-      carListbox.setModel(new ListModelList<Car>(result));
-   }
+      List< Car > result = carService.search( keyword );
 
-   @Listen("onSelect = #carListbox")
-	public void showDetail(){
-      detailBox.setVisible(true);
+      //identify current selection
+      Optional< Car > existingSelection = extractCurrentModel()
+            .flatMap( this::retrieveCurrentSelection );
 
-		Set<Car> selection = ((Selectable<Car>)carListbox.getModel()).getSelection();
-		if (selection!=null && !selection.isEmpty()){
-         Car selected = selection.iterator().next();
-         previewImage.setSrc(selected.getPreview());
-         modelLabel.setValue(selected.getModel());
-         makeLabel.setValue(selected.getMake());
-         priceLabel.setValue(selected.getPrice().toString());
-         descriptionLabel.setValue(selected.getDescription());
+      ListModelList< Car > searchResultModel = new ListModelList<>( result );
+      carListbox.setModel( searchResultModel );
+
+      //restore selection if possible
+      if ( existingSelection.isPresent() && result.contains( existingSelection.get() ) ) {
+         searchResultModel.setSelection( singleton( existingSelection.get() ) );
       }
+
+      showDetailForCurrentSelection();
    }
 
-   @Listen("onCreate = #carListbox")
+   /**
+    * Shows the detail of the current selection, if there is one.
+    */
+   @Listen( "onSelect = #carListbox" )
+   public void showDetailForCurrentSelection() {
+      ListModelList< Car > model = extractCurrentModel()
+            .orElseThrow( () -> new IllegalStateException(
+                  "Application only supports ListModelList."
+            ) );
+
+      Optional< Car > currentSelection = retrieveCurrentSelection( model );
+      if ( !currentSelection.isPresent() ) {
+         //nothing to select, hide detail
+         detailBox.setVisible( false );
+         return;
+      }
+
+      if ( !model.contains( currentSelection.get() ) ) {
+         //our selection is no longer in the model - maybe due to filtering, hide detail
+         detailBox.setVisible( false );
+         return;
+      }
+
+      Car selected = currentSelection.get();
+      detailBox.setVisible( true );
+      previewImage.setSrc( selected.getPreview() );
+      modelLabel.setValue( selected.getModel() );
+      makeLabel.setValue( selected.getMake() );
+      priceLabel.setValue(
+            Optional.ofNullable( selected.getPrice() )
+                  .map( Objects::toString )
+                  .orElse( "Unknown" ) );
+      descriptionLabel.setValue( selected.getDescription() );
+   }
+
+   /**
+    * Convenience method to retrieve the {@link ListModelList} expected from the car listbox. It
+    * is expected that this application only uses {@link ListModelList} however on start up, a
+    * system default may be provided which will subsequently be replaced.
+    * @return the model expected or empty if it has not been set yet.
+    */
+   private Optional< ListModelList< Car > > extractCurrentModel() {
+      ListModel< ? > model = carListbox.getModel();
+      if ( model == null ) {
+         return Optional.empty();
+      }
+
+      if ( model instanceof ListModelList ) {
+         return Optional.ofNullable( ( ListModelList< Car > ) model );
+      }
+
+      return Optional.empty();
+   }
+
+   /**
+    * Retrieves the current selection from the given model.
+    * @param model to retrieve the selection from.
+    * @return the current selection, if there is one. Note that this expects that multi selection
+    * is not possible.
+    */
+   private Optional< Car > retrieveCurrentSelection( ListModelList< Car > model ) {
+      if ( model == null || model.isEmpty() ) {
+         return Optional.empty();
+      }
+
+      Set< Car > selection = model.getSelection();
+      if ( selection == null || selection.isEmpty() ) {
+         return Optional.empty();
+      }
+      return Optional.ofNullable( selection.iterator().next() );
+   }
+
+   /**
+    * Called on window load. Provides any initialisation for this controller or default 
+    * information to the user.
+    */
+   @Listen( "onCreate = #carListbox" )
    public void windowLoad() {
       search();
+   }
+
+   /*
+    * Ideally we would not have these setters. It's not clear how best to test these elements.
+    * There's possibility for wiring on construction, or using set methods, but no success with
+    * this thus far. Mockito is capable of these types of things so this should be able to be
+    * improved.
+    */
+
+   void setCarListbox( Listbox carListbox ) {
+      this.carListbox = carListbox;
+   }
+
+   void setDescriptionLabel( Label descriptionLabel ) {
+      this.descriptionLabel = descriptionLabel;
+   }
+
+   void setDetailBox( Component detailBox ) {
+      this.detailBox = detailBox;
+   }
+
+   void setKeywordBox( Textbox keywordBox ) {
+      this.keywordBox = keywordBox;
+   }
+
+   void setMakeLabel( Label makeLabel ) {
+      this.makeLabel = makeLabel;
+   }
+
+   void setModelLabel( Label modelLabel ) {
+      this.modelLabel = modelLabel;
+   }
+
+   void setPreviewImage( Image previewImage ) {
+      this.previewImage = previewImage;
+   }
+
+   void setPriceLabel( Label priceLabel ) {
+      this.priceLabel = priceLabel;
    }
 }
---
+++ src/test/java/demo/getting_started/mvc/SearchControllerTest.java
@@ -0,0 +1,190 @@
+// **************************************************
+//                        GSDK                       
+//           Graffica System Development Kit         
+//                                                   
+//  Release Version: {RELEASE_VERSION}               
+//  Copyright: (c) Graffica Ltd {RELEASE_DATE}       
+//                                                   
+// **************************************************
+//  This software is provided under the terms of the 
+//        Graffica Software Licence Agreement        
+//                                                   
+//     THIS HEADER MUST NOT BE ALTERED OR REMOVED    
+// **************************************************
+
+package demo.getting_started.mvc;
+
+import demo.getting_started.model.services.CarService;
+import demo.getting_started.model.structures.Car;
+import demo.getting_started.model.structures.SortableColour;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zul.*;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singleton;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith( MockitoExtension.class )
+public class SearchControllerTest {
+
+   private static final String SEARCH_CRITERIA = "anything";
+
+   @Captor
+   private ArgumentCaptor< ListModelList< Car > > modelCaptor;
+
+   @Mock
+   private Textbox keywordBox;
+   @Mock
+   private Listbox carListbox;
+   @Mock
+   private Label modelLabel;
+   @Mock
+   private Label makeLabel;
+   @Mock
+   private Label priceLabel;
+   @Mock
+   private Label descriptionLabel;
+   @Mock
+   private Image previewImage;
+   @Mock
+   private Component detailBox;
+
+   @Mock
+   private CarService carService;
+   private SearchController systemUnderTest;
+
+   @BeforeEach
+   public void initialiseSystemUnderTest() {
+      systemUnderTest = new SearchController( carService );
+
+      systemUnderTest.setKeywordBox( keywordBox );
+      systemUnderTest.setCarListbox( carListbox );
+      systemUnderTest.setModelLabel( modelLabel );
+      systemUnderTest.setMakeLabel( makeLabel );
+      systemUnderTest.setPriceLabel( priceLabel );
+      systemUnderTest.setDescriptionLabel( descriptionLabel );
+      systemUnderTest.setPreviewImage( previewImage );
+      systemUnderTest.setDetailBox( detailBox );
+   }
+
+   @Test
+   public void shouldUpdateModelWithSearchResults() {
+      List< Car > searchResult = asList( mock( Car.class ) );
+      when( carService.search( SEARCH_CRITERIA ) ).thenReturn( searchResult );
+      when( keywordBox.getValue() ).thenReturn( SEARCH_CRITERIA );
+      when( carListbox.getModel() ).thenReturn( new ListModelList<>( searchResult ) );
+
+      systemUnderTest.search();
+
+      verify( carListbox ).setModel( modelCaptor.capture() );
+      assertThat( modelCaptor.getValue().getInnerList(), equalTo( searchResult ) );
+   }
+
+   @Test
+   public void shouldRestoreSelectionWhenSearching() {
+      Car selected = new Car();
+      List< Car > searchResult = asList( selected );
+      when( carService.search( SEARCH_CRITERIA ) ).thenReturn( searchResult );
+      when( keywordBox.getValue() ).thenReturn( SEARCH_CRITERIA );
+
+      ListModelList< Object > modelList = new ListModelList<>( searchResult );
+      modelList.setSelection( singleton( selected ) );
+      when( carListbox.getModel() ).thenReturn( modelList );
+
+      systemUnderTest.search();
+
+      verify( carListbox ).setModel( modelCaptor.capture() );
+      assertThat( modelCaptor.getValue().getSelection(), containsInAnyOrder( selected ) );
+   }
+
+   @Test
+   public void shouldNotRestoreSelectionWhenSearchResultsDoNotContainSelection() {
+      Car selected = new Car();
+      List< Car > searchResult = asList( new Car() );
+      when( carService.search( SEARCH_CRITERIA ) ).thenReturn( searchResult );
+      when( keywordBox.getValue() ).thenReturn( SEARCH_CRITERIA );
+
+      ListModelList< Object > modelList = new ListModelList<>( searchResult );
+      modelList.setSelection( singleton( selected ) );
+      when( carListbox.getModel() ).thenReturn( modelList );
+
+      systemUnderTest.search();
+
+      verify( carListbox ).setModel( modelCaptor.capture() );
+      assertThat( modelCaptor.getValue().getSelection(), empty() );
+   }
+
+   @Test
+   public void shouldRetainDetailShownFollowingSearch() {
+      shouldRestoreSelectionWhenSearching();
+      verify( detailBox ).setVisible( true );
+   }
+
+   @Test
+   public void shouldHideDetailShowFollowingSearch() {
+      shouldNotRestoreSelectionWhenSearchResultsDoNotContainSelection();
+      verify( detailBox ).setVisible( false );
+   }
+
+   @Test
+   public void shouldShowDetailWhenSelectionOccurs() {
+      Car selected = new Car( 21, "Model", "Make", "Description", "Preview", 20000,
+            new SortableColour( Color.WHITE, "White" )
+      );
+
+      ListModelList< Object > modelList = new ListModelList<>( singleton( selected ) );
+
+      modelList.setSelection( singleton( selected ) );
+      when( carListbox.getModel() ).thenReturn( modelList );
+
+      systemUnderTest.showDetailForCurrentSelection();
+      verify( detailBox ).setVisible( true );
+      verify( previewImage ).setSrc(selected.getPreview());
+      verify( modelLabel ).setValue(selected.getModel());
+      verify( makeLabel ).setValue(selected.getMake());
+      verify( priceLabel ).setValue("20000");
+      verify( descriptionLabel ).setValue(selected.getDescription());
+   }
+
+   @Test
+   public void shouldHideDetailWhenNoSelectionPresent() {
+      Car notSelected = new Car( 21, "Model", "Make", "Description", "Preview", 20000,
+            new SortableColour( Color.WHITE, "White" )
+      );
+
+      ListModelList< Object > modelList = new ListModelList<>( singleton( notSelected ) );
+
+      modelList.setSelection( emptyList() );
+      when( carListbox.getModel() ).thenReturn( modelList );
+
+      systemUnderTest.showDetailForCurrentSelection();
+      verify( detailBox ).setVisible( false );
+   }
+
+   @Test
+   public void shouldProvideAllDataWhenLoaded() {
+      List< Car > searchResult = asList( mock( Car.class ) );
+      when( carService.search( "" ) ).thenReturn( searchResult );
+      when( keywordBox.getValue() ).thenReturn( "" );
+      when( carListbox.getModel() ).thenReturn( new ListModelList<>( searchResult ) );
+
+      systemUnderTest.windowLoad();
+
+      verify( carListbox ).setModel( modelCaptor.capture() );
+      assertThat( modelCaptor.getValue().getInnerList(), equalTo( searchResult ) );
+   }
+
+}
